f6170214f64368b7e43c792040e78f5173bc1992
Amanraj0001 <130846248+Amanraj0001@users.noreply.github.com>
2026-08-31 23:39:30 +0530

    Time: 0 ms (100%), Space: 9.9 MB (85.78%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
@@ -1,46 +1,13 @@
 class Solution {
 public:
-    vector<vector<vector<int>>> dp;
-
-    int solve(vector<int>& nums, int i, int prev, int chk) {
-        int n = nums.size();
-
-        if (i >= n)
-            return 0;
-
-        int p = prev + 1;
-
-        if (dp[i][p][chk] != -1)
-            return dp[i][p][chk];
-
-        int take = 0;
-
-        if (chk == 1) {
-            if (nums[i] > prev) {
-                take = 1 + solve(nums, i + 1, nums[i], 0);
-            }
-        } else {
-            if (nums[i] < prev) {
-                take = 1 + solve(nums, i + 1, nums[i], 1);
-            }
-        }
-
-        int skip = solve(nums, i + 1, prev, chk);
-
-        return dp[i][p][chk] = max(take, skip);
-    }
-
     int wiggleMaxLength(vector<int>& nums) {
-        int n = nums.size();
-
-        if (n == 0)
-            return 0;
-
-        dp.assign(n, vector<vector<int>>(1002, vector<int>(2, -1)));
-
-        int ans1 = 1 + solve(nums, 1, nums[0], 0);
-        int ans2 = 1 + solve(nums, 1, nums[0], 1);
-
-        return max(ans1, ans2);
+        int up=0;
+        int down=0;
+        bool ctr=true;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]-nums[i+1]>0)down=up+1;
+            else if(nums[i]-nums[i+1]<0)up=down+1;
+        }
+        return max(up,down)+1;
     }
 };
