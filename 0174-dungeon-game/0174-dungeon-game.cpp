3525632a9b7d7b406a3b9bb7c385e165d729478e
Amanraj0001 <130846248+Amanraj0001@users.noreply.github.com>
2026-08-31 21:35:22 +0530

    Time: 1 ms (25.88%), Space: 13.1 MB (52.07%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.cpp
+++ 0174-dungeon-game/0174-dungeon-game.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
+    int solve(vector<vector<int>>& d, vector<vector<int>>& dp, int i, int j) {
+        int n = d.size();
+        int m = d[0].size();
+
+        if (i == n - 1 && j == m - 1) {
+            return max(1, 1 - d[i][j]);
+        }
+
+        if (i >= n || j >= m)
+            return INT_MAX;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        int right = solve(d, dp, i, j + 1);
+        int down = solve(d, dp, i + 1, j);
+
+        int next = min(right, down);
+
+        return dp[i][j] = max(1, next - d[i][j]);
+    }
+
     int calculateMinimumHP(vector<vector<int>>& d) {
         int n = d.size();
         int m = d[0].size();
 
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, INT_MAX));
+        vector<vector<int>> dp(n, vector<int>(m, -1));
 
-        dp[n][m - 1] = 1;
-        dp[n - 1][m] = 1;
-
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = m - 1; j >= 0; j--) {
-
-                int need = min(dp[i + 1][j], dp[i][j + 1]) - d[i][j];
-
-                dp[i][j] = max(1, need);
-            }
-        }
-
-        return dp[0][0];
+        return solve(d, dp, 0, 0);
     }
 };
